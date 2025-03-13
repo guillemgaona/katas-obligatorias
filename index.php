e5c0cc70fd1b4a509e4680c4c7d1fc268544344c
guillemgaona <guillemgaona@hotmail.com>
2025-03-13 12:05:14 +0100

    Manejo errores

--- index.php
+++ index.php
@@ -2,21 +2,30 @@
 /*Escriu un algoritme que llegeixi un nombre enter i determini si és parell o imparell.
 
 Si és parell, que escrigui tots els parells de manera descendent des de si mateix fins a 0.
-Si és imparell, que escrigui tots els parells de manera descendent des de si mateix fins a l'1.
+Si és imparell, que escrigui tots els imparells de manera descendent des de si mateix fins a l'1.
 El nombre que entri al programa ha d'entrar directament a la variable "number".*/
 
-function evenOrOdd(int $number): void {
-
-    if($number % 2 == 0) {
-        for ($i = $number; $i >=0; $i-=2) {
-            echo $i . ", ";
-        } 
-    } else if($number % 2 !== 0) {
-        for ($i = $number; $i >=1; $i-=2) {
-            echo $i . ", ";
+function evenOrOdd($number): void {
+    if (filter_var($number, FILTER_VALIDATE_INT) && $number > 0) {
+        if($number % 2 == 0) {
+            for ($i = $number; $i >=0; $i-=2) {
+                echo $i . "<br> ";
+            } 
+        } else if($number % 2 !== 0) {
+            for ($i = $number; $i >=1; $i-=2) {
+                echo $i . "<br>";
+            
+            }
+        }
+    } else if ($number == 0) {
+        echo 0;
+    } else {
+        echo "escriu un nombre vàlid";
     }
-    } 
 }
 
 evenOrOdd(19);
-evenOrOdd(34);
+evenOrOdd("a");
+evenOrOdd(0);
+evenOrOdd(-3);
+evenOrOdd("44");
